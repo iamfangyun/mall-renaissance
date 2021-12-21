3702730739508cdbf40503513da404a9beeef73e
方雲 <930154711@qq.com>
2021-12-21 15:52:23 +0800

    Create AdminApp.java

---
+++ mall-admin/src/main/java/com/renaissance/admin/AdminApp.java
@@ -0,0 +1,19 @@
+package com.renaissance.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能描述
+ *
+ * @author weishanyuan
+ * @since 2021-10-14
+ */
+@SpringBootApplication
+public class AdminApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApp.class, args);
+    }
+
+}
