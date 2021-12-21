536dc40887f1a712f0eb7da9dc6db95405a7b01b
方雲 <930154711@qq.com>
2021-12-21 16:16:30 +0800

    Create CmsPrefrenceAreaService.java

---
+++ mall-admin/src/main/java/com/renaissance/admin/service/CmsPrefrenceAreaService.java
@@ -0,0 +1,17 @@
+package com.renaissance.admin.service;
+
+import com.renaissance.entity.CmsPrefrenceArea;
+
+import java.util.List;
+
+/**
+ * 功能描述
+ *
+ * @author weishanyuan
+ * @since 2021-10-14
+ */
+public interface CmsPrefrenceAreaService {
+
+    List<CmsPrefrenceArea> listAll();
+
+}
